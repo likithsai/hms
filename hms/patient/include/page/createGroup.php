fcc6e5454236628452471c3d6ea3ae6f363f3f47
likith sai <likithsai13@gmail.com>
2020-07-06 12:46:58 +0530

    minor changes

---
+++ hms/patient/include/page/createGroup.php
@@ -0,0 +1,41 @@
+<?php
+    include('../include/config.php');
+?>
+
+<!-- The Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Create Group</h4>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        <div class="form-group">
+            <label for="email">Enter Group Name: </label>
+            <input type="text" class="form-control" placeholder="Enter Group Name">
+        </div>
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-info" data-dismiss="modal">Submit</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="wrap-content container" id="container">
+	<div class="container-fluid container-fullw bg-white">
+        
+        <div class="row">
+            <button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal">Create Group</button>    
+        </div>
+    
+    </div>
+</div>
